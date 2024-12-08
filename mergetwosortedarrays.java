18a204a54848549849fb154c6da1655500647548
vina786 <vinay76b@gmail.com>
2024-12-08 14:28:43 +0530

    Add files via upload

---
+++ mergetwosortedarrays.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class mergetwosortedarrays 
+{
+    public static void main(String args[])
+    {
+        int arr1[]={3,4,5,6};
+        int arr2[]={9,10,12};
+        int k=0;
+        int temp[]=new int[arr1.length+arr2.length];
+        for(int i=0;i<arr1.length;i++)
+        {
+            temp[k]=arr1[i];
+            k++;
+        }
+        for(int i=0;i<arr2.length;i++)
+        {
+            temp[k]=arr2[i];
+            k++;
+        }
+        System.out.println(Arrays.toString(temp));
+        }
+    }
+    
